3fa2fae7ce66e56cb04ae095151c294a35d5b7e7
fl-y <thedigitalimg@gmail.com>
2022-07-31 02:35:38 +0900

    fix: precommit hooks

--- components/common/DisplayJson.tsx
+++ components/common/DisplayJson.tsx
@@ -1,7 +1,8 @@
 import dynamic from 'next/dynamic';
+import { FunctionComponent } from 'react';
 
 const ReactJson = dynamic(() => import('react-json-view'), { ssr: false });
 
-export const DisplayJson = ({ data }) => {
+export const DisplayJson: FunctionComponent<{ data: any }> = ({ data }) => {
 	return <ReactJson theme="monokai" src={data} collapseStringsAfterLength={20} enableClipboard />;
 };
